4d3905fa44906f61f11606d0cc6d799e21789a91
Hysterelius <71912794+Hysterelius@users.noreply.github.com>
2023-02-25 16:16:45 +1100

    feat: :lock: Created a more secure CSP
    Used hashes to create a more secure csp, updated packages

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       "Strict-Transport-Security": "max-age=86400; includeSubDomains",
       "X-Frame-Options": "DENY",
       "X-XSS-Protection": "1; mode=block",
+      'Content-Security-Policy': "script-src 'self' 'sha256-mmaBgWlsOVoMkqC5s+vKzLFJ/t/iy7w8P7jX0WUe4Uw='; style-src 'self' 'sha256-xQU2JFaFDXi4FCb1ytEGKWxhTPxvVs20Qzw7cMmTGBI=' 'sha256-9wekLdNYkqDuT3Z6/y2jedOEGaJo10K8OM+nbjwARD0=' 'sha256-a6n8BhVV0lFqCoOJ+NcvaksZvjwpNFYBVKoM4Adr55Y=' 'sha256-OZ+0sGyazgOzwxhk9mK5LpqTUORAkPP3fBM0pN/bobE='",
     },
   },
 });
