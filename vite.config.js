cfb64d5c2fe647b831d069e8ab389339e4229dac
Hysterelius <71912794+Hysterelius@users.noreply.github.com>
2025-02-22 12:28:56 +1100

    Update action and deps

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
       "Strict-Transport-Security": "max-age=86400; includeSubDomains",
       "X-Frame-Options": "DENY",
       "X-XSS-Protection": "1; mode=block",
-      'Content-Security-Policy': "script-src 'self' 'sha256-mmaBgWlsOVoMkqC5s+vKzLFJ/t/iy7w8P7jX0WUe4Uw='; style-src 'self' 'sha256-GtjAc689pnmCaBh6Imq7HOm1IcOefnSBYaUEjmTCIvI=' 'sha256-bESg7MVKSOA3sl63nMw2+gOqAUTpYZRTpzhDbxjeBjU=' 'sha256-2FpZhhgM+U+GSxs8WvrjoWXyJ3J0/1V8k7cuBvYvVNc='",
+      // 'Content-Security-Policy': "script-src 'self' 'sha256-mmaBgWlsOVoMkqC5s+vKzLFJ/t/iy7w8P7jX0WUe4Uw='; style-src 'self' 'sha256-GtjAc689pnmCaBh6Imq7HOm1IcOefnSBYaUEjmTCIvI=' 'sha256-bESg7MVKSOA3sl63nMw2+gOqAUTpYZRTpzhDbxjeBjU=' 'sha256-2FpZhhgM+U+GSxs8WvrjoWXyJ3J0/1V8k7cuBvYvVNc='",
     },
   },
 });
